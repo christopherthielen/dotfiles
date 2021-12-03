eac60848920e8ceab58e1e055d8efc33fe4d05c1
Chris Thielen <cthielen@netflix.com>
2021-12-02 15:27:14 -1000

    install asimov

--- dev/post-install.sh
+++ dev/post-install.sh
@@ -11,3 +11,5 @@ rm -rf "$JENV_VERSIONS" && mkdir -p "$JENV_VERSIONS"
 
 for java in $(find /Library/Java/JavaVirtualMachines -mindepth 1 -maxdepth 1 ! -type l); do yes | (jenv add "$java/Contents/Home" > /dev/null); done
 (jenv rehash &) 2> /dev/null
+
+brew services start asimov
