8b88a1a18a6fe20d8d570c4cc2c8e4f74051663f
Chris Thielen <cthielen@netflix.com>
2021-12-02 15:34:12 -1000

    install asimov service (using sudo)

--- dev/post-install.sh
+++ dev/post-install.sh
@@ -12,4 +12,4 @@ rm -rf "$JENV_VERSIONS" && mkdir -p "$JENV_VERSIONS"
 for java in $(find /Library/Java/JavaVirtualMachines -mindepth 1 -maxdepth 1 ! -type l); do yes | (jenv add "$java/Contents/Home" > /dev/null); done
 (jenv rehash &) 2> /dev/null
 
-brew services start asimov
+sudo brew services start asimov
