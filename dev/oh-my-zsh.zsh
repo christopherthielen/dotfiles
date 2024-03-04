819b46c600525f2b348e13bd066cee89a6c19971
Chris Thielen <github@sandgnat.com>
2024-03-04 09:45:34 -1000

    remove custom nvm script, configure nvm plugin to lazy load

--- dev/oh-my-zsh.zsh
+++ dev/oh-my-zsh.zsh
@@ -3,3 +3,6 @@
 FZF_BIN=/opt/homebrew/bin/fzf
 FZF_REALPATH=$(realpath $FZF_BIN)
 export FZF_BASE=$(dirname $FZF_REALPATH)/..
+
+# Reduce startup time of nvm plugin
+zstyle ':omz:plugins:nvm' lazy yes
