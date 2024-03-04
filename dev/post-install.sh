434425bd39b664b002f93186a4f40950c0f71e7d
Chris Thielen <github@sandgnat.com>
2024-03-04 10:46:08 -1000

    improve startup speed of 'yarn' plugin by wiring a global yarn dir ~/.yarn/bin

--- dev/post-install.sh
+++ dev/post-install.sh
@@ -12,6 +12,7 @@ rm -rf "$JENV_VERSIONS" && mkdir -p "$JENV_VERSIONS"
 for java in $(find /Library/Java/JavaVirtualMachines -mindepth 1 -maxdepth 1 ! -type l); do yes | (jenv add "$java/Contents/Home" > /dev/null); done
 (jenv rehash &) 2> /dev/null
 
+# asimov excludes node_modules, etc from time machine
 if brew services list | grep -q "^asimov *stopped" ; then
   sudo brew services start asimov
 fi
@@ -19,3 +20,8 @@ fi
 # Install vim plugins
 luarocks install nvim-lspconfig
 nvim -c PlugInstall -c qa --headless
+
+# Configure "global yarn bin" directory for faster yarn plugin startup
+mkdir -p ~/.yarn
+[[ -e ~/.yarn/bin ]] && rm ~/.yarn/bin
+ln -s /opt/homebrew/bin ~/.yarn/bin
