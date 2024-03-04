cc577987cfc07be18f8f26b81545b8f615162cd7
Chris Thielen <github@sandgnat.com>
2024-03-04 11:05:16 -1000

    Do not load jenv at startup

--- dev/post-install.sh
+++ dev/post-install.sh
@@ -1,17 +1,4 @@
 set -e
-
-JENV="$HOME/.jenv"
-JENV_VERSIONS="$JENV/versions"
-mkdir -p "$JENV"
-
-# cleanup to avoid intermittent failures from rehashing
-
-find "$JENV" -type f -name '*.time' -delete
-rm -rf "$JENV_VERSIONS" && mkdir -p "$JENV_VERSIONS"
-
-for java in $(find /Library/Java/JavaVirtualMachines -mindepth 1 -maxdepth 1 ! -type l); do yes | (jenv add "$java/Contents/Home" > /dev/null); done
-(jenv rehash &) 2> /dev/null
-
 # asimov excludes node_modules, etc from time machine
 if brew services list | grep -q "^asimov *stopped" ; then
   sudo brew services start asimov
