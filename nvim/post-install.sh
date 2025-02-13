b7c2646a3b480a1f6da44188b0931542d83c746b
Chris Thielen <cthielen@netflix.com>
2025-02-13 12:53:19 -1000

    Nvim: Fix updating kickstart from github, add some additional plugins

--- nvim/post-install.sh
+++ nvim/post-install.sh
@@ -1,16 +1,25 @@
+set -ex
 KICKSTART_PATH="${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
-if [[ -e "$KICKSTART_PATH/.git ]] ; then
+if [[ -e "$KICKSTART_PATH/.git" ]] ; then
   pushd $KICKSTART_PATH
   git pull --rebase
   popd
-elif [[ -e "$KICKSTART_PATH ]] ; then
-  WARNING: Cannot install nvim-lua/kickstart.nvim because $KICKSTART_PATH already has files
+elif [[ -e "$KICKSTART_PATH" ]] ; then
+  echo WARNING: Cannot install nvim-lua/kickstart.nvim because $KICKSTART_PATH already has files
 else
   git clone https://github.com/nvim-lua/kickstart.nvim.git "$KICKSTART_PATH"
 fi
 
 # Enable custom plugin loading from ~/.config/nvim/lua/custom/plugins/
-sed -i '' -e "s/^  -- { import = 'custom.plugins' },/  { import = 'custom.plugins' },/" "$KICKSTART_PATH/init.lua"
+sed -i '' "/^  -- { import = 'custom.plugins' }/s/-- //" "$KICKSTART_PATH/init.lua"
+# Enable neo-tree
+sed -i '' "/^vim.g.have_nerd_font = false/s/false/true/" "$KICKSTART_PATH/init.lua"
+sed -i '' "/^  -- require 'kickstart.plugins.neo-tree/s/--//"  "$KICKSTART_PATH/init.lua"
+# Enable other plugins kickstart includes
+sed -i '' "/^  -- require 'kickstart.plugins.indent_line/s/--//"  "$KICKSTART_PATH/init.lua"
+sed -i '' "/^  -- require 'kickstart.plugins.lint/s/--//"  "$KICKSTART_PATH/init.lua"
+sed -i '' "/^  -- require 'kickstart.plugins.autopairs/s/--//"  "$KICKSTART_PATH/init.lua"
+
 nvim --headless "+Lazy! sync" +qa
 
