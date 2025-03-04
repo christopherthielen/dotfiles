f44ce5c6cf5de6beb1b2b7bbc40b5df515fa5df3
Chris Thielen <cthielen@netflix.com>
2025-03-03 14:18:00 -1000

    nvim: install via github release, not apt (jammy had old version 0.6.1)

---
+++ nvim/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod 755 nvim-linux-x86_64.appimage
+sudo chown root:root nvim-linux-x86_64.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
