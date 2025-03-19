928a7c294760ff2dff1ed01010865a2a0033985f
Chris Thielen <cthielen@netflix.com>
2025-03-19 13:33:54 -1000

    Always update nvim kickstart from upstream

--- nvim/post-install.sh
+++ nvim/post-install.sh
@@ -1,14 +1,15 @@
 set -ex
 KICKSTART_PATH="${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
-if [[ -e "$KICKSTART_PATH/.git" ]] ; then
-  pushd $KICKSTART_PATH
-  git pull --rebase
-  popd
-elif [[ -e "$KICKSTART_PATH" ]] ; then
-  echo WARNING: Cannot clone nvim-lua/kickstart.nvim because $KICKSTART_PATH already has files
-else
+if [[ ! -e "$KICKSTART_PATH/.git" ]] ; then
   ln -s "$HOME/.kickstart.nvim" "$KICKSTART_PATH"
 fi
+
+pushd $KICKSTART_PATH
+git remote add upstream https://github.com/nvim-lua/kickstart.nvim.git || true
+git fetch
+git pull --rebase
+git rebase upstream/master
+popd
 
 nvim --headless "+Lazy! sync" +qa
