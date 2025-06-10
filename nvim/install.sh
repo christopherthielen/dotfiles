f12530371b8e4ab6219965d227345f493c10e522
Chris Thielen <cthielen@netflix.com>
2025-06-10 09:40:04 -1000

    nvim: Don't install linux binary unless on linux

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
-chmod 755 nvim-linux-x86_64.appimage
-sudo chown root:root nvim-linux-x86_64.appimage
-sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+if [ "$(uname)" = "Linux" ] ; then
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+  chmod 755 nvim-linux-x86_64.appimage
+  sudo chown root:root nvim-linux-x86_64.appimage
+  sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+fi
