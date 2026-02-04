a96a7cb51665b89bbe8f4b7489ba4582ff081a71
Chris Thielen <github@sandgnat.com>
2026-02-03 17:36:16 -1000

    Dotfiles audit: remove unused configs and packages
    Removed:
    - dircolors-solarized (using lsd instead)
    - DEFAULT_USER export (not needed with Starship)
    - OMZL::git.zsh, OMZP::fzf, OMZP::brew (redundant/replaced)
    - Duplicate fzf loading in tools.zsh
    - Git functions: prunegitlocalbranches, git-pr, git-prune-merged
    - git plb2 alias and prune-merged-pull-requests script
    - Scripts: gradle-exorcize, node-inspect
    - Yarn bin symlink setup (not needed for yarn v1)
    - Yubikey from Linux setup (not needed on remote workstations)
    - Packages: ripgrep, dos2unix, gradle-completion, postgresql, jetbrains-toolbox
    - Vestige ignore rules: .bashrc, .oh-my-zsh/cache
    
    Changed:
    - top alias now uses htop (cross-platform)
    - fzf uses native shell integration

--- dot_config/zsh/git.zsh
+++ dot_config/zsh/git.zsh
@@ -1,10 +1,5 @@
 # Git helper functions
 
-# Delete branches that have been merged
-function git-prune-merged() {
-    git branch --merged | grep -v "\*" | grep -v "main" | grep -v "master" | xargs -n 1 git branch -d
-}
-
 # Show git status in all subdirectories
 function git-status-all() {
     for d in */; do
@@ -15,17 +10,7 @@ function git-status-all() {
     done
 }
 
-# Checkout PR by number
-function git-pr() {
-    git fetch origin "pull/$1/head:pr-$1" && git checkout "pr-$1"
-}
-
 # Show commit count by author
 function scorecard() {
     git log --pretty=%an | sort | uniq -c | sort -r
-}
-
-# Prune local branches tracking deleted remotes
-function prunegitlocalbranches() {
-    git remote prune origin && git branch -vv | grep '\[[^]]* gone\]' | awk '{ print $1 }' | xargs git branch -D
 }
